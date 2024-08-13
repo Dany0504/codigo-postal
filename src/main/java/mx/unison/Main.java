32434ba36ddbe3216a10ff5151a243cbbac1134c
Dany0504 <a223206520@unison.mx>
2024-08-13 15:36:07 -0700

    listo

--- src/main/java/mx/unison/Main.java
+++ src/main/java/mx/unison/Main.java
@@ -1,4 +1,7 @@
 package mx.unison;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 /* Contar cuantos códigos postales corresponden a asentamientos rurales
 y cantos a asentamientos rurales
@@ -7,6 +10,27 @@ y cantos a asentamientos rurales
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        String archivo = "codigos_postales.csv";
+        String linea;
+        int urbanos = 0;
+        int rurales = 0;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+            br.readLine();
+            while((linea = br.readLine()) != null) {
+                String[] datos = linea.split(",");
+                String Asentamiento = datos[2].trim().toLowerCase();
+                if (Asentamiento.equals("urbano")){
+                    urbanos++;
+                } else if (Asentamiento.equals("rural")) {
+                    rurales++;
+                }
+            }
+        }catch (IOException e){
+            System.out.println("Error al leer el archivo");
+        }
+        System.out.println("Asentamientos urbanos: " + urbanos);
+        System.out.println("Asentamientos rurales: " + rurales);
+
     }
 }
